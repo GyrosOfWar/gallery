701c7720c9d829286f86d3d95023583dc5abdb8c
Martin Tomasi <martin.tomasi@gmail.com>
2023-02-12 23:19:34 +0100

    prettier

--- src/main/java/com/github/gyrosofwar/imagehive/controller/ImageController.java
+++ src/main/java/com/github/gyrosofwar/imagehive/controller/ImageController.java
@@ -1,20 +1,19 @@
 package com.github.gyrosofwar.imagehive.controller;
 
-import com.github.gyrosofwar.imagehive.sql.Tables;
+import static com.github.gyrosofwar.imagehive.sql.Tables.IMAGE;
+
 import com.github.gyrosofwar.imagehive.sql.tables.pojos.Image;
 import io.micronaut.http.MediaType;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
 import io.micronaut.http.annotation.PathVariable;
-import org.jooq.DSLContext;
-
-import javax.transaction.Transactional;
 import java.util.UUID;
-
-import static com.github.gyrosofwar.imagehive.sql.Tables.IMAGE;
+import javax.transaction.Transactional;
+import org.jooq.DSLContext;
 
 @Controller("/api/images")
 public class ImageController {
+
   private final DSLContext dsl;
 
   public ImageController(DSLContext dsl) {
@@ -24,8 +23,6 @@ public class ImageController {
   @Get(produces = MediaType.APPLICATION_JSON, uri = "/{uuid}")
   @Transactional
   public Image getImage(@PathVariable UUID uuid) {
-    return dsl.selectFrom(IMAGE)
-      .where(IMAGE.ID.eq(uuid))
-      .fetchOneInto(Image.class);
+    return dsl.selectFrom(IMAGE).where(IMAGE.ID.eq(uuid)).fetchOneInto(Image.class);
   }
 }
