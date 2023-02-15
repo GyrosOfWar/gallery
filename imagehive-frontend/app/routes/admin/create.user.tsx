c489bc9a14b5ec09f6ad4ac280ae4802928fd78c
Rubikan <rubikandreas@gmail.com>
2023-02-15 13:12:29 +0100

    added first version of create user form, removed yarn.lock

---
+++ imagehive-frontend/app/routes/admin/create.user.tsx
@@ -0,0 +1,89 @@
+import {ActionArgs, json, LoaderArgs} from "@remix-run/node";
+import {authenticator} from "~/services/auth.server";
+import {Form} from "@remix-run/react";
+import {Button, Checkbox, Label, TextInput} from "flowbite-react";
+
+export async function action({request}: ActionArgs) {
+    return await authenticator.authenticate("user-pass", request, {
+        successRedirect: "/admin/create/user",
+        failureRedirect: "/auth/login",
+    })
+}
+
+export async function loader({request}: LoaderArgs) {
+    const response = await authenticator.isAuthenticated(request, {
+        successRedirect: "/admin/create/user",
+    })
+
+    return json(response)
+}
+
+const CreateUser = () => {
+    return (
+        <Form
+            className="flex flex-col gap-4 max-w-2xl w-full self-center"
+            method="post"
+        >
+            <h1 className="text-3xl font-bold">Create User</h1>
+
+            <div>
+                <Label className="mb-2 block" htmlFor="username">
+                    Username
+                </Label>
+                <TextInput
+                    id="username"
+                    placeholder="Enter username..."
+                    type="text"
+                    name="username"
+                    required
+                />
+            </div>
+            <div>
+                <Label className="mb-2 block" htmlFor="generatePassword">
+                    Generate password
+                </Label>
+                <Checkbox
+                    id="generatePassword"
+                    name="generatePassword"
+                />
+            </div>
+            <div>
+                <Label className="mb-2 block" htmlFor="password">
+                    Password
+                </Label>
+                <TextInput
+                    id="password"
+                    placeholder="Enter password..."
+                    type="password"
+                    name="password"
+                    required
+                    autoComplete="current-password"
+                />
+            </div>
+            <div>
+                <Label className="mb-2 block" htmlFor="email">
+                    E-Mail
+                </Label>
+                <TextInput
+                    id="email"
+                    placeholder="Enter email..."
+                    type="text"
+                    name="email"
+                    required
+                />
+            </div>
+            <div>
+                <Label className="mb-2 block" htmlFor="admin">
+                    Admin
+                </Label>
+                <Checkbox
+                    id="admin"
+                    name="admin"
+                />
+            </div>
+            <Button type="submit">Create</Button>
+        </Form>
+    )
+}
+
+export default CreateUser
