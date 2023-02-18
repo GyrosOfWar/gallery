f8874123cf94a9209d7ea386e81acc13a5284bdd
Martin Tomasi <martin.tomasi@gmail.com>
2023-02-18 08:30:07 +0100

    move env vars

--- build.sh
+++ build.sh
@@ -1,10 +1,6 @@
 #!/usr/bin/env sh
 set -e
 
-export IMAGEHIVE_PG_JDBC_URL=jdbc:postgresql://localhost:5432/$1
-export IMAGEHIVE_PG_USER=$2
-export IMAGEHIVE_PG_PASSWORD=$3
-
 ./mvnw flyway:migrate
 ./mvnw compile
 npx @openapitools/openapi-generator-cli generate
