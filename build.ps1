83d8ccff544138a7b62ebed0d3d6a52d720ace2a
Rubikan <rubikandreas@gmail.com>
2023-02-20 20:10:18 +0100

    rewrote .ps scripts so database config can be set either via env or argument

--- build.ps1
+++ build.ps1
@@ -1,7 +1,8 @@
-$env:IMAGEHIVE_PG_JDBC_URL=$args[0]
-$env:IMAGEHIVE_PG_USER=$args[1]
-$env:IMAGEHIVE_PG_PASSWORD=$args[2]
-
+if (!(Test-Path "env:IMAGEHIVE_PG_JDBC_URL") -and !(Test-Path "env:IMAGEHIVE_PG_USER") -and !(Test-Path "env:IMAGEHIVE_PG_PASSWORD")) {
+    $env:IMAGEHIVE_PG_JDBC_URL=$args[0]
+    $env:IMAGEHIVE_PG_USER=$args[1]
+    $env:IMAGEHIVE_PG_PASSWORD=$args[2]
+}
 ./mvnw.bat flyway:migrate
 ./mvnw.bat compile
 npx @openapitools/openapi-generator-cli generate
