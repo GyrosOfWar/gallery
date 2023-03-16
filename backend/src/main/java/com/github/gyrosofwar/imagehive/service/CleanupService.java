485fc81dc3e752234f02f7ffa1303de3e2726106
Rubikan <rubikandreas@gmail.com>
2023-03-16 04:50:13 +0100

    exception handling in case no images have been uploaded yet

--- backend/src/main/java/com/github/gyrosofwar/imagehive/service/CleanupService.java
+++ backend/src/main/java/com/github/gyrosofwar/imagehive/service/CleanupService.java
@@ -4,6 +4,7 @@ import io.micronaut.context.annotation.Requires;
 import io.micronaut.scheduling.annotation.Scheduled;
 import jakarta.inject.Singleton;
 import java.io.IOException;
+import java.nio.file.NoSuchFileException;
 import me.desair.tus.server.TusFileUploadService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -22,7 +23,14 @@ public class CleanupService {
 
   @Scheduled(fixedRate = "10m")
   public void cleanupUploadedFiles() throws IOException {
-    log.info("cleaning up uploaded files");
-    fileUploadService.cleanup();
+    log.info("Cleaning up temporary image uploads");
+    try {
+      fileUploadService.cleanup();
+    } catch (NoSuchFileException e) {
+      // This happens if no files have been uploaded yet, but log it in debug so it won't get lost
+      if (log.isDebugEnabled()) {
+        log.debug("There was an error cleaning up temporary image uploads", e);
+      }
+    }
   }
 }
