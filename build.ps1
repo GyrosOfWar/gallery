4219208755617e61f9b1fcdab5fcb76f4aaf915d
Andreas Rubik <rubikandreas@gmail.com>
2023-09-26 11:13:11 +0200

    added way to customize java home just for build

--- build.ps1
+++ build.ps1
@@ -1,7 +1,10 @@
 if (!(Test-Path "env:IMAGEHIVE_PG_JDBC_URL") -and !(Test-Path "env:IMAGEHIVE_PG_USER") -and !(Test-Path "env:IMAGEHIVE_PG_PASSWORD")) {
-    $env:IMAGEHIVE_PG_JDBC_URL=$args[0]
-    $env:IMAGEHIVE_PG_USER=$args[1]
-    $env:IMAGEHIVE_PG_PASSWORD=$args[2]
+    $env:IMAGEHIVE_PG_JDBC_URL = $args[0]
+    $env:IMAGEHIVE_PG_USER = $args[1]
+    $env:IMAGEHIVE_PG_PASSWORD = $args[2]
+}
+if (Test-Path -Path .\JAVA_HOME -PathType Leaf) {
+    $env:JAVA_HOME = Get-Content -Path .\JAVA_HOME
 }
 Set-Location backend
 ./mvnw.bat flyway:migrate
